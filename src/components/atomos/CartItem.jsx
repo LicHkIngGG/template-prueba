77f0d92d1209b54d6c98cf9c9129bb97f4ef4c58
LichKinGgg <caperto303@gmail.com>
2023-11-22 23:50:39 -0400

    holis 3

--- src/components/atomos/CartItem.jsx
+++ src/components/atomos/CartItem.jsx
@@ -4,17 +4,20 @@ import "./styles/CartItem.css";
 function CartItem({ title, quantity, price, description, imageUrl }) {
   return (
     <div className="cart-item">
-      <div className="cart-item-image-container">
-        <img src={imageUrl} alt={title} className="cart-item-image" />
-      </div>
-      <div className="cart-item-info">
-        <h3 className="cart-item-title">{title}</h3>
-        <p className="cart-item-quantity">{`Cantidad ${quantity} unidades`}</p>
-        <p className="cart-item-description">{description}</p>
-        <p className="cart-item-price">{`Bs ${price}`}</p>
+      <img src={imageUrl} alt={title} className="cart-item-image" />
+      <div className="cart-item-content">
+        <div className="cart-item-header">
+          <h3 className="cart-item-title">{title}</h3>
+          <span className="cart-item-price">{`Bs ${price}`}</span>
+        </div>
+        <div className="cart-item-body">
+          <p className="cart-item-quantity">{`Cantidad ${quantity} unidades`}</p>
+          <p className="cart-item-description">{description}</p>
+        </div>
       </div>
     </div>
   );
 }
 
 export default CartItem;
+
